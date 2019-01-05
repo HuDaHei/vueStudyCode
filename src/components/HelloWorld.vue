0c8261151b740f7f15459a9bfe96c625efabba40
HuDaHei <1305544590@qq.com>
2019-01-05 18:03:56 +0800

    vue生命周期验证

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+<div>{{com}} 周期阶段{{msg}}</div>
+</template>
+
+<script>
+console.log('start')
+export default {
+  name: 'HelloWorld',
+  data () {
+    console.log("data")
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    com() {
+      console.log("computed");
+      return "computed"
+    }
+  },
+  watch: {
+    msg () {
+      console.log("watch")
+    }
+  },
+  beforeCreate () {
+     console.log("beforeCreate")
+  },
+  created () {
+    this.msg = "createWatch"
+    console.log("created")
+  },
+  beforeMount () {
+     console.log("beforeMount")   
+  },
+  mounted () {
+      console.log("mounted");
+      setTimeout(()=>{
+        this.msg = "更新";
+      }, 3000)
+   
+  },
+  updated() {
+    console.log("update")
+  }
+}
+</script>
---
+++ src/view/list/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="item in routerList" :key="item.path">
+        <router-link  :to="item.path">{{item.name}}</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      routerList: [
+        { path: '/', name: '首页' },
+        {
+          path: '/HelloWorld',
+          name: '生命周期顺序'
+        }
+      ]
+    }
+  }
+}
+</script>
