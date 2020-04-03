f283bd70f7abd3396d1e7e0a0d7284ca095430df
韩军 <hanjun@hanjundeMacBook-Pro.local>
2020-04-03 14:32:11 +0800

    函数式组件

--- src/view/performance/func.vue
+++ src/view/performance/func.vue
@@ -1,6 +1,7 @@
 <template functional>
 <div>
-  <div>kkk</div>
-  <div>{{props.test}}</div>
+  <div>{{props.test}} 我是props</div>
+  <slot name="one"/>
+  <slot name="two"/>
 </div>
 </template>
--- src/view/performance/functional.vue
+++ src/view/performance/functional.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     我是function
-    <cl-func test="test"></cl-func>
+    <cl-func test="test" kk="color:red">
+      <template v-slot:one>
+        <p>one</p>
+        <p>two</p>
+      </template>
+      <p>three</p>
+      <template v-slot:two>
+        <p>five</p>
+        <p>six</p>
+      </template>
+    </cl-func>
   </div>
 </template>
 <script>
-import func from './func'
+import func from "./func";
 export default {
-  name: 'functionalll',
-  components:{
-    'cl-func': func,
+  name: "functionalll",
+  components: {
+    "cl-func": func
+  },
+  data() {
+    return {
+      styles: {
+        color: "red"
+      }
+    };
   }
-}
+};
 </script>
