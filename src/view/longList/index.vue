e3ec5c05e92185ea204db3fa07dcab7d9916f8ed
DESKTOP-A8FIUDD\兜里 <1305544590@qq.com>
2020-09-22 17:00:36 +0800

    长列表

--- src/view/longList/index.vue
+++ src/view/longList/index.vue
@@ -1,64 +1,104 @@
 <template>
   <div>
-    <table class="long-table" border="1" border-co>
+    <button @click="handlerAddData">add</button>
+    <table class="long-table" border="1">
       <thead>
         <tr>
-          <th>kkeeeekkkeeeekkkeeeekkkeeeek</th>
-          <th>kksk</th>
-          <th>kksk</th>
-          <th>kksk</th>
-          <th>kksk</th>
-          <th>kksk</th>
-          <th>kksk</th>
-          <th>kksk</th>
-          <th>kksk</th>
+          <td>23</td>
         </tr>
       </thead>
-      <tbody class="long-table_tbody">
-        <template v-for="item in test ">
-          <tr :key="item.id">
-            <template v-for="(key, index) in keys">
-              <td :key="index">{{ item[key] }}</td>
-            </template>
-          </tr>
-        </template>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colspan="5">kk</td>
-        </tr>
-      </tfoot>
     </table>
+    <div class="long_table_tbody" @scroll="hanlderScroll">
+      <table class="long-table" border="1">
+        <tbody class="long-table_tbody" id="long_table_tbody" :style="tbodyHeight">
+          <template v-for="item in tableData ">
+            <tr :key="item.id">
+              <template v-for="(key, index) in keys">
+                <td :key="index">{{ item[key] }}</td>
+              </template>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import test from './data'
+import test from "./data";
 export default {
   name: "longList",
   data() {
     return {
       keys: ["name", "age", "sex", "email", "address"],
-      test: Object.freeze(test)
+      test: test,
+      tableData: [],
+      startDivIndex:0,
+      endDivIndex: 0
     };
+  },
+  computed:{
+    tbodyHeight() {
+      return {
+        height: `${this.test.length * 21}px`
+      }
+    }
+  },
+  mounted() {
+    this.watchDomSizeChange();
+    this.init();
   },
   methods: {
     name() {
       Reflect;
     },
+    init(){
+      this.endDivIndex = Math.ceil(200 / 21);
+      const test = JSON.parse(JSON.stringify(this.test));
+      this.tableData = test.splice(0, this.endDivIndex)
+    },
+    handlerAddData() {
+      const temp = {
+        name: Math.random(),
+        age: "kkk",
+        sex: "kkk",
+        email: "kkk",
+        address: "kkk",
+        id: Math.random(),
+      };
+      this.test.push(temp);
+    },
+    watchDomSizeChange() {
+      const resize = new ResizeObserver((entry) => {
+        console.log(entry, "entrt");
+      });
+      resize.observe(document.querySelector("#long_table_tbody"));
+    },//
+    hanlderScroll(e){
+      const target = e.target;
+      const scrollTop = target.scrollTop;
+      const showDataCount = Math.ceil(scrollTop / 21);
+      const test = JSON.parse(JSON.stringify(this.test));
+      const data = test.splice(this.endDivIndex, showDataCount);
+      this.endDivIndex = this.endDivIndex+showDataCount;
+      let  oldData = JSON.parse(JSON.stringify(this.tableData));
+      // oldData.splice(0,showDataCount)
+      this.tableData = [...oldData, ...data]
+    },//
   },
 };
 </script>
 
 <style>
-thead,tbody{
-    display: block;
+.long-table {
+  border-collapse: collapse;
 }
-.long-table{
-    border-collapse: collapse;
+.long_table_tbody {
+  height: 200px;
+  display: inline-block;
+  overflow: auto;
 }
-.long-table_tbody{
-    height: 100px;
-    overflow: overlay;
+.long-table_tbody {
+  display: inline-block;
 }
 </style>
